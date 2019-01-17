c5acbcf8e15a688bb667963933efe727cdf8c903
VolantTyler <33227593+VolantTyler@users.noreply.github.com>
2019-01-17 16:42:30 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The number could not be compiled.");
+            }
+        }
+    }
+}
